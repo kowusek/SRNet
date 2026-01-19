02fc6baeae082f483e271026b03760597d1ce206
Maciej Kowalski <maciej.kowalski@promity.com>
2026-01-19 13:22:14 +0100

    Updated Neural Network to resemble paper a bit more.

--- src/srnet/models/components/sr_dense_net.py
+++ src/srnet/models/components/sr_dense_net.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 
 
 class ConvNode(nn.Module):
-    def __init__(self, input_channels, output_channels, kernel_size, padding):
-        super().__init__()
+    def __init__(self, input_channels, output_channels, kernel_size, padding, b_norm):
+        super(ConvNode, self).__init__()
+        self.b_norm = b_norm
         self.add_module(
             "conv",
             nn.Conv2d(
@@ -16,22 +17,37 @@ class ConvNode(nn.Module):
             ),
         )
         self.add_module("relu", nn.ReLU(inplace=True))
-        # self.add_module('norm', nn.BatchNorm2d(output_channels))
-        nn.init.xavier_uniform_(self.conv.weight, gain=nn.init.calculate_gain("relu"))
+        if b_norm:
+            self.add_module("norm", nn.BatchNorm2d(output_channels))
+        else:
+            nn.init.xavier_uniform_(
+                self.conv.weight, gain=nn.init.calculate_gain("relu")
+            )
 
     def forward(self, input):
-        return self.relu(self.conv(input))  # self.norm(self.relu(self.conv(input)))
+        if self.b_norm:
+            return self.norm(self.relu(self.conv(input)))
+        else:
+            return self.relu(self.conv(input))
 
 
 class DenseBlock(nn.ModuleDict):
-    def __init__(self, num_layers, channels, kernel_size, padding):
-        super().__init__()
+    def __init__(self, num_layers, channels, growth_rate, kernel_size, padding, b_norm):
+        super(DenseBlock, self).__init__()
         for i in range(num_layers):
-            layer = ConvNode(channels * (i + 1), channels, kernel_size, padding)
+            layer = ConvNode(
+                channels + growth_rate * i, growth_rate, kernel_size, padding, b_norm
+            )
             self.add_module("conv%d" % i, layer)
         self.add_module(
             "last_conv",
-            ConvNode((num_layers + 1) * channels, channels, kernel_size=1, padding=0),
+            nn.Conv2d(
+                channels + num_layers * growth_rate,
+                channels,
+                kernel_size=1,
+                padding=0,
+                bias=False,
+            ),
         )
 
     def forward(self, input):
@@ -40,19 +56,36 @@ class DenseBlock(nn.ModuleDict):
             if name[:4] == "conv":
                 new_features = layer(features)
                 features = torch.cat([features, new_features], 1)
-        features = self.last_conv(features)
-        return features + input
+        features = self.last_conv(features) + input
+        return features
 
 
 class GlobalDenseBlock(nn.ModuleDict):
-    def __init__(self, num_dense_blocks, num_layers, channels, kernel_size, padding):
-        super().__init__()
+    def __init__(
+        self,
+        num_dense_blocks,
+        num_layers,
+        channels,
+        growth_rate,
+        kernel_size,
+        padding,
+        b_norm,
+    ):
+        super(GlobalDenseBlock, self).__init__()
         for i in range(num_dense_blocks):
-            rdb = DenseBlock(num_layers, channels, kernel_size, padding)
+            rdb = DenseBlock(
+                num_layers, channels, growth_rate, kernel_size, padding, b_norm
+            )
             self.add_module("rdb%d" % i, rdb)
         self.add_module(
             "last_conv",
-            ConvNode(num_dense_blocks * channels, channels, kernel_size=1, padding=0),
+            nn.Conv2d(
+                num_dense_blocks * channels,
+                channels,
+                kernel_size=1,
+                padding=0,
+                bias=False,
+            ),
         )
 
     def forward(self, input):
@@ -61,7 +94,7 @@ class GlobalDenseBlock(nn.ModuleDict):
         for name, layer in self.items():
             if name[:3] == "rdb":
                 features = layer(features)
-                if all_features is not None:
+                if all_features != None:
                     all_features = torch.cat([all_features, features], 1)
                 else:
                     all_features = features
@@ -73,34 +106,52 @@ class DenseNet(nn.ModuleDict):
     def __init__(
         self,
         input_channels=3,
-        num_dense_blocks=4,
-        num_layers=3,
+        scale_factor=4,
+        num_dense_blocks=20,
+        num_layers=6,
         channels=64,
+        growth_rate=32,
         kernel_size=3,
         padding=1,
-        upscale_factor=4,
+        b_norm=False,
     ):
-        super().__init__()
+        super(DenseNet, self).__init__()
         self.add_module(
-            "conv1", ConvNode(input_channels, channels, kernel_size=3, padding=1)
+            "conv1",
+            nn.Conv2d(input_channels, channels, kernel_size=3, padding=1, bias=False),
         )
-        self.add_module("conv2", ConvNode(channels, channels, kernel_size=3, padding=1))
+        self.add_module(
+            "conv2", nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        )
         self.add_module(
             "global_dense",
             GlobalDenseBlock(
-                num_dense_blocks, num_layers, channels, kernel_size, padding
+                num_dense_blocks,
+                num_layers,
+                channels,
+                growth_rate,
+                kernel_size,
+                padding,
+                b_norm,
             ),
         )
-        self.add_module("conv3", ConvNode(channels, channels, kernel_size=3, padding=1))
-        self.add_module("upscale", nn.PixelShuffle(upscale_factor))
+        self.add_module(
+            "conv3", nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        )
+        self.add_module("upscale", nn.PixelShuffle(scale_factor))
         self.add_module(
             "conv4",
-            ConvNode(
-                channels // upscale_factor**2,
-                input_channels,
-                kernel_size=1,
-                padding=0,
+            nn.Conv2d(
+                channels // scale_factor**2,
+                channels,
+                kernel_size=3,
+                padding=1,
+                bias=False,
             ),
+        )
+        self.add_module(
+            "conv5",
+            nn.Conv2d(channels, input_channels, kernel_size=1, padding=0, bias=False),
         )
 
     def forward(self, input):
@@ -110,6 +161,7 @@ class DenseNet(nn.ModuleDict):
         features = self.conv3(features) + input
         features = self.upscale(features)
         features = self.conv4(features)
+        features = self.conv5(features)
         return features
 
 
